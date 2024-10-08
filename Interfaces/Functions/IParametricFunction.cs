7987be87c9ca0aa574bc35cd636f7099a5550ee7
shishnk <akorund@mail.ru>
2024-10-08 23:11:51 +0700

    Have added tests for optimizers

--- Interfaces/Functions/IParametricFunction.cs
+++ Interfaces/Functions/IParametricFunction.cs
@@ -1,12 +1,8 @@
 ﻿using Interfaces.DataStorage;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Interfaces.Functions;
-public interface IParametricFunction<TFunction> where TFunction : IFunction
+
+public interface IParametricFunction<out TFunction> where TFunction : IFunction
 {
     TFunction Bind(IVector parameters);
 }
